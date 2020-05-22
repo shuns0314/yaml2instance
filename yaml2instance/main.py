061425a009c96bea7a297643176c686521fd0ab1
shuns0314 <shunsuke.naka0314@gmail.com>
2020-05-22 14:53:21 +0900

    Add main logic

---
+++ yaml2instance/main.py
@@ -0,0 +1,34 @@
+"""Main."""
+from typing import Union, List
+
+
+def yaml2instances(
+    loading_data: Union[dict, List[dict]], search_modules: list, **kwargs
+) -> list:
+    """Pareser of the loading_data of loading yaml and the output is an instance."""
+    instance_list = []
+    if isinstance(loading_data, list):
+        for conf in loading_data:
+            for instance in _loading_data_to_instance(conf, search_modules, **kwargs):
+                instance_list.append(instance)
+    elif isinstance(loading_data, dict):
+        for instance in _loading_data_to_instance(
+            loading_data, search_modules, **kwargs
+        ):
+            instance_list.append(instance)
+    else:
+        TypeError
+
+    return instance_list
+
+
+def _loading_data_to_instance(loading_data: dict, search_modules, **kwargs):
+    modules_dict = {}
+    for _item, _kwargs in loading_data.items():
+        if _kwargs is not None:
+            kwargs.update(_kwargs)
+        for module in search_modules:
+            _modules = module.__dict__
+            modules_dict.update(_modules)
+        instance = modules_dict[_item](**(kwargs or {}))
+        yield instance
